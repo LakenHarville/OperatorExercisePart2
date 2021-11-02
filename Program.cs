48566685ef7fb436a3a03c52eae2e563183ec519
Laken Harville <rlakenharville@gmail.com>
2021-11-01 22:35:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperatorExercisePart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(Math.PI, radius);
+            Console.WriteLine(area);
+        }
+
+        public static double AreaOfCircle(double Pi, double radius)
+        {
+            double area = Pi * (radius * radius);
+            return area;
+        }
+        
+    }
+}
